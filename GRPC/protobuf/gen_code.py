fbe4178e76f0aae2dadcce501dca0908487a2392
Marvin Gravert <marvin.gravert@gmail.com>
2020-11-21 02:42:36 +0100

    fuck grpc

--- GRPC/protobuf/gen_code.py
+++ GRPC/protobuf/gen_code.py
@@ -13,8 +13,8 @@ from itertools import chain
 
 def generate_proto_code():
     """
-    Generates proto code and returns list of generated pyton modules
-    Return: list of generated modules as strings
+    Generates proto code and returns list of file names
+    Return: list of file names
     """
     """
     Get file names
@@ -29,11 +29,11 @@ def generate_proto_code():
     Create Folder
     based on filename
     """
-    for file_name in file_name_list:
-        try:
-            mkdir(file_name)
-        except FileExistsError:
-            pass
+    folder_name = "protodef_files"
+    try:
+        mkdir(folder_name)
+    except FileExistsError:
+        pass
 
     """
     Generate the code into the folder
@@ -42,8 +42,8 @@ def generate_proto_code():
         protoc.main([
             'grpc_tools.protoc',
             '--proto_path=protos',
-            f'--python_out={file_name}',
-            f'--grpc_python_out={file_name}',
+            f'--python_out={folder_name}',
+            f'--grpc_python_out={folder_name}',
             file_path])
     """
     Adjust the import statement
@@ -55,17 +55,16 @@ def generate_proto_code():
     Instead the interpreter needs to be told to look in the same current dir:
     from . import client_stream_pb2 as client__stream__pb2
     """
-    for file_name in file_name_list:
-        for script in glob.iglob(f"./{file_name}/*.py"):
-            with open(script, 'r+') as file:
-                code = file.read()
-                file.seek(0)
-                file.write(re.sub(r'(import .+_pb2.*)', 'from . \\1', code))
-                file.truncate()
 
+    for script in glob.iglob(f"./{folder_name}/*.py"):
+        with open(script, 'r+') as file:
+            code = file.read()
+            file.seek(0)
+            file.write(re.sub(r'(import .+_pb2.*)', 'from . \\1', code))
+            file.truncate()
     return list(chain.from_iterable(
-        [[f"{name}_pb2", f"{name}_pb2_grpc"] for name in file_name_list]))
+                [f"{name}_pb2", f"{name}_pb2_grpc"] for name in file_name_list))
 
 
 if __name__ == "__main__":
-    generate_proto_code()
+    print(generate_proto_code())
--- GRPC/protobuf/setup.py
+++ GRPC/protobuf/setup.py
@@ -1,9 +1,11 @@
+from distutils import filelist
 from setuptools import setup, find_packages
 
-import gen_code
+from gen_code import generate_proto_code
+import os
 
-gen_code.generate_proto_code()
-
+py_modules = generate_proto_code()
+os.chdir("./protodef_files")
 setup(
     name='protobufRepo',  # name which will be used to install via pip
     version='0.1',  # version number simple as that
@@ -14,7 +16,8 @@ setup(
 
     license='MIT',
     # which directories to search for imports. Importable dirs are marked by
-    packages=find_packages(),
-    # packages=find_packages()#also possible but may include unwanted dir such as tests
+    py_modules=py_modules,
+    # also possible but may include unwanted dir such as tests
+    # packages=["protodef_files"],
     zip_safe=False,
 )
